d312e68b558efbeda19f8db798104ddb07ec6a8c
HubbePlatteau <hubbe.platteau@student.vives.be>
2022-12-31 16:23:53 +0100

    Mainpage voorbereid op Semester2 en Home knop

---
+++ assets/Java.js
@@ -0,0 +1,9 @@
+/*const button = document.querySelector(".button2");
+
+button.addEventListener("click", () => {
+    if (button.style.display == "") {
+        button.style.backgroundColor = "gray";
+    }
+})
+
+*/
